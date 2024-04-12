6b8bb719a289391365640dc469062c64b68bf499
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-04-11 22:18:24 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Escolha6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxSerpente1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSerpente2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSerpente3 = new System.Windows.Forms.PictureBox();
+            this.radioButtonSerpe1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSerpen2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSerpen3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.pictureBoxLagarto1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLagarto2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLatarto3 = new System.Windows.Forms.PictureBox();
+            this.radioButtonLagar1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonLagar2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonLagar3 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSerpente1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSerpente2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSerpente3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLagarto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLagarto2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLatarto3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxSerpente1
+            // 
+            this.pictureBoxSerpente1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSerpente1.Image")));
+            this.pictureBoxSerpente1.Location = new System.Drawing.Point(65, 45);
+            this.pictureBoxSerpente1.Name = "pictureBoxSerpente1";
+            this.pictureBoxSerpente1.Size = new System.Drawing.Size(201, 184);
+            this.pictureBoxSerpente1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSerpente1.TabIndex = 0;
+            this.pictureBoxSerpente1.TabStop = false;
+            this.pictureBoxSerpente1.Visible = false;
+            this.pictureBoxSerpente1.WaitOnLoad = true;
+            this.pictureBoxSerpente1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBoxSerpente2
+            // 
+            this.pictureBoxSerpente2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSerpente2.Image")));
+            this.pictureBoxSerpente2.Location = new System.Drawing.Point(65, 248);
+            this.pictureBoxSerpente2.Name = "pictureBoxSerpente2";
+            this.pictureBoxSerpente2.Size = new System.Drawing.Size(201, 184);
+            this.pictureBoxSerpente2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSerpente2.TabIndex = 1;
+            this.pictureBoxSerpente2.TabStop = false;
+            this.pictureBoxSerpente2.Visible = false;
+            // 
+            // pictureBoxSerpente3
+            // 
+            this.pictureBoxSerpente3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSerpente3.Image")));
+            this.pictureBoxSerpente3.Location = new System.Drawing.Point(65, 449);
+            this.pictureBoxSerpente3.Name = "pictureBoxSerpente3";
+            this.pictureBoxSerpente3.Size = new System.Drawing.Size(201, 184);
+            this.pictureBoxSerpente3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSerpente3.TabIndex = 2;
+            this.pictureBoxSerpente3.TabStop = false;
+            this.pictureBoxSerpente3.Visible = false;
+            // 
+            // radioButtonSerpe1
+            // 
+            this.radioButtonSerpe1.AutoSize = true;
+            this.radioButtonSerpe1.Location = new System.Drawing.Point(326, 152);
+            this.radioButtonSerpe1.Name = "radioButtonSerpe1";
+            this.radioButtonSerpe1.Size = new System.Drawing.Size(113, 24);
+            this.radioButtonSerpe1.TabIndex = 3;
+            this.radioButtonSerpe1.TabStop = true;
+            this.radioButtonSerpe1.Text = "Serpente 1";
+            this.radioButtonSerpe1.UseVisualStyleBackColor = true;
+            this.radioButtonSerpe1.Visible = false;
+            // 
+            // radioButtonSerpen2
+            // 
+            this.radioButtonSerpen2.AutoSize = true;
+            this.radioButtonSerpen2.Location = new System.Drawing.Point(326, 331);
+            this.radioButtonSerpen2.Name = "radioButtonSerpen2";
+            this.radioButtonSerpen2.Size = new System.Drawing.Size(113, 24);
+            this.radioButtonSerpen2.TabIndex = 4;
+            this.radioButtonSerpen2.TabStop = true;
+            this.radioButtonSerpen2.Text = "Serpente 2";
+            this.radioButtonSerpen2.UseVisualStyleBackColor = true;
+            this.radioButtonSerpen2.Visible = false;
+            // 
+            // radioButtonSerpen3
+            // 
+            this.radioButtonSerpen3.AutoSize = true;
+            this.radioButtonSerpen3.Location = new System.Drawing.Point(326, 528);
+            this.radioButtonSerpen3.Name = "radioButtonSerpen3";
+            this.radioButtonSerpen3.Size = new System.Drawing.Size(113, 24);
+            this.radioButtonSerpen3.TabIndex = 5;
+            this.radioButtonSerpen3.TabStop = true;
+            this.radioButtonSerpen3.Text = "Serpente 3";
+            this.radioButtonSerpen3.UseVisualStyleBackColor = true;
+            this.radioButtonSerpen3.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(100, 12);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(108, 24);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Serpentes";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // pictureBoxLagarto1
+            // 
+            this.pictureBoxLagarto1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLagarto1.Image")));
+            this.pictureBoxLagarto1.Location = new System.Drawing.Point(554, 45);
+            this.pictureBoxLagarto1.Name = "pictureBoxLagarto1";
+            this.pictureBoxLagarto1.Size = new System.Drawing.Size(201, 184);
+            this.pictureBoxLagarto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLagarto1.TabIndex = 7;
+            this.pictureBoxLagarto1.TabStop = false;
+            this.pictureBoxLagarto1.Visible = false;
+            this.pictureBoxLagarto1.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBoxLagarto2
+            // 
+            this.pictureBoxLagarto2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLagarto2.Image")));
+            this.pictureBoxLagarto2.Location = new System.Drawing.Point(554, 248);
+            this.pictureBoxLagarto2.Name = "pictureBoxLagarto2";
+            this.pictureBoxLagarto2.Size = new System.Drawing.Size(201, 184);
+            this.pictureBoxLagarto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLagarto2.TabIndex = 8;
+            this.pictureBoxLagarto2.TabStop = false;
+            this.pictureBoxLagarto2.Visible = false;
+            // 
+            // pictureBoxLatarto3
+            // 
+            this.pictureBoxLatarto3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLatarto3.Image")));
+            this.pictureBoxLatarto3.Location = new System.Drawing.Point(554, 449);
+            this.pictureBoxLatarto3.Name = "pictureBoxLatarto3";
+            this.pictureBoxLatarto3.Size = new System.Drawing.Size(201, 184);
+            this.pictureBoxLatarto3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLatarto3.TabIndex = 9;
+            this.pictureBoxLatarto3.TabStop = false;
+            this.pictureBoxLatarto3.Visible = false;
+            // 
+            // radioButtonLagar1
+            // 
+            this.radioButtonLagar1.AutoSize = true;
+            this.radioButtonLagar1.Location = new System.Drawing.Point(786, 143);
+            this.radioButtonLagar1.Name = "radioButtonLagar1";
+            this.radioButtonLagar1.Size = new System.Drawing.Size(102, 24);
+            this.radioButtonLagar1.TabIndex = 10;
+            this.radioButtonLagar1.TabStop = true;
+            this.radioButtonLagar1.Text = "Lagarto 1";
+            this.radioButtonLagar1.UseVisualStyleBackColor = true;
+            this.radioButtonLagar1.Visible = false;
+            // 
+            // radioButtonLagar2
+            // 
+            this.radioButtonLagar2.AutoSize = true;
+            this.radioButtonLagar2.Location = new System.Drawing.Point(786, 331);
+            this.radioButtonLagar2.Name = "radioButtonLagar2";
+            this.radioButtonLagar2.Size = new System.Drawing.Size(102, 24);
+            this.radioButtonLagar2.TabIndex = 11;
+            this.radioButtonLagar2.TabStop = true;
+            this.radioButtonLagar2.Text = "Lagarto 2";
+            this.radioButtonLagar2.UseVisualStyleBackColor = true;
+            this.radioButtonLagar2.Visible = false;
+            // 
+            // radioButtonLagar3
+            // 
+            this.radioButtonLagar3.AutoSize = true;
+            this.radioButtonLagar3.Location = new System.Drawing.Point(786, 527);
+            this.radioButtonLagar3.Name = "radioButtonLagar3";
+            this.radioButtonLagar3.Size = new System.Drawing.Size(102, 24);
+            this.radioButtonLagar3.TabIndex = 12;
+            this.radioButtonLagar3.TabStop = true;
+            this.radioButtonLagar3.Text = "Lagarto 3";
+            this.radioButtonLagar3.UseVisualStyleBackColor = true;
+            this.radioButtonLagar3.Visible = false;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(573, 11);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(97, 24);
+            this.radioButton8.TabIndex = 13;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Lagartos";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 642);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(776, 642);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 710);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButtonLagar3);
+            this.Controls.Add(this.radioButtonLagar2);
+            this.Controls.Add(this.radioButtonLagar1);
+            this.Controls.Add(this.pictureBoxLatarto3);
+            this.Controls.Add(this.pictureBoxLagarto2);
+            this.Controls.Add(this.pictureBoxLagarto1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButtonSerpen3);
+            this.Controls.Add(this.radioButtonSerpen2);
+            this.Controls.Add(this.radioButtonSerpe1);
+            this.Controls.Add(this.pictureBoxSerpente3);
+            this.Controls.Add(this.pictureBoxSerpente2);
+            this.Controls.Add(this.pictureBoxSerpente1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSerpente1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSerpente2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSerpente3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLagarto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLagarto2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLatarto3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxSerpente1;
+        private System.Windows.Forms.PictureBox pictureBoxSerpente2;
+        private System.Windows.Forms.PictureBox pictureBoxSerpente3;
+        private System.Windows.Forms.RadioButton radioButtonSerpe1;
+        private System.Windows.Forms.RadioButton radioButtonSerpen2;
+        private System.Windows.Forms.RadioButton radioButtonSerpen3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.PictureBox pictureBoxLagarto1;
+        private System.Windows.Forms.PictureBox pictureBoxLagarto2;
+        private System.Windows.Forms.PictureBox pictureBoxLatarto3;
+        private System.Windows.Forms.RadioButton radioButtonLagar1;
+        private System.Windows.Forms.RadioButton radioButtonLagar2;
+        private System.Windows.Forms.RadioButton radioButtonLagar3;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escolha6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxSerpente1.Visible = true;
+            radioButtonSerpe1.Visible = true;
+            pictureBoxSerpente2.Visible = true;
+            radioButtonSerpen2.Visible = true;
+            pictureBoxSerpente3.Visible = true;
+            radioButtonSerpen3.Visible = true;
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxLagarto1.Visible = true;
+            radioButtonLagar1.Visible = true;
+            pictureBoxLagarto2.Visible = true;
+            radioButtonLagar2.Visible = true;
+            pictureBoxLatarto3.Visible = true;
+            radioButtonLagar3.Visible = true;
+        }
+    }
+}
